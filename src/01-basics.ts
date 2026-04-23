4e4af4173157d7371dd2614ec52b199d3ffaea47
samAd0san <sohaibsamad.hala@gmail.com>
2026-04-23 18:09:01 +0530

    feat: Initialise typescript project 1. npx tsx --init 2. Compile -> tsc + enter -> node 01-basics.ts

---
+++ src/01-basics.ts
@@ -0,0 +1,13 @@
+/**
+ * To run this file, first write 
+ * 1. tsc + enter
+ * 2. node 01-basics.ts
+ * output:
+ * 5 Hello world! typescript!!
+ */
+
+
+let num: number = 5;
+let message: string = "Hello world! typescript!!"
+
+console.log(num,message);
