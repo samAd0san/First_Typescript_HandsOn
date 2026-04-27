4d158e24d00e02906c2cad9fd8ee4d7e214e9fcd
samAd0san <sohaibsamad.hala@gmail.com>
2026-04-27 15:19:13 +0530

    feat: Type inference (ts gets the type even if we not define it)

---
+++ src/03-type-inference.ts
@@ -0,0 +1,7 @@
+// Typesscript infers the type, i.e it gets the type by itself if we do not declare the type
+
+let num = 10;
+let string = "This is a String"
+
+// string = 10; // Error - Type 'number' is not assignable to type 'string'.
+// num = "10" // Error - Type 'string' is not assignable to type 'number'.
